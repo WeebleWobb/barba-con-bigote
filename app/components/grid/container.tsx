e5f70a33834a0aaa4079ee1ff1139c0577669559
Juan D. Bolaños <juanb@aweber.com>
2025-02-27 17:28:05 -0500

    Updates Container component styling

--- app/components/grid/container.tsx
+++ app/components/grid/container.tsx
@@ -11,31 +11,14 @@ type ContainerProps = {
 }
 
 const Container = ({ className = '', children }: ContainerProps) => {
-  const pathname = usePathname()
-  
-  const getPageColor = () => {
-    switch(pathname) {
-      case '/trabajo':
-        return 'bg-dribble-pink'
-      case '/cerveza':
-        return 'bg-cerveza-crema'
-      case '/musica':
-        return 'bg-spotify-black'
-      default:
-        return 'bg-red'
-    }
-  }
-
-  const classes = twMerge(
-    clsx(
-      getPageColor(),
-      'basis-full flex items-start lg:items-center',
-    ),
-    className
-  )
 
   return (
-    <main className={classes}>
+    <main className={
+      twMerge(
+        'bg-red basis-full flex items-start lg:items-center', 
+        className
+      )
+    }>
       <div className='pt-8 px-6 sm:flex-auto md:pb-10 md:px-16'>
         {children}
       </div>
