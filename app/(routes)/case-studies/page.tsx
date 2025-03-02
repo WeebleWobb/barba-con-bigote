103148dfe6f63de374be3caad1074da2199ce550
Juan D. Bolaños <juanb@aweber.com>
2025-03-02 14:59:52 -0500

    Sets up page and content transition

--- app/(routes)/case-studies/page.tsx
+++ app/(routes)/case-studies/page.tsx
@@ -1,63 +1,61 @@
-import Content from '@/components/grid/container'
+'use client'
+
+import Container from '@/components/grid/container'
 import Row from '@/components/grid/row'
 import Column from '@/components/grid/column'
 import CaseStudyCard from '@/components/casestudy-card'
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Case Studies',
-  description: 'A collection of case studies, component specs, and projects that showcase my more than a decades worth of experience in the design industry.',
-  openGraph: {
-    title: 'Case Studies',
-    description: 'A collection of case studies, component specs, and projects that showcase my more than a decades worth of experience in the design industry.'
-  }
-}
+import ContentTransition from '@/components/motion/content-transition'
 
 const caseStudyEntries = [
   {
     title: 'Case Study 1',
     image: 'https://placehold.co/600x400',
-    link: '/trabajo/case-study-1'
+    link: '/case-studies/case-study-1'
   },
   {
     title: 'Case Study 2',
     image: 'https://placehold.co/600x400',
-    link: '/trabajo/case-study-2'
+    link: '/case-studies/case-study-2'
   },
   {
     title: 'Case Study 3',
     image: 'https://placehold.co/600x400',
-    link: '/trabajo/case-study-3'
+    link: '/case-studies/case-study-3'
   }
 ]
+
 const CaseStudies = () => {
   return (
-    <Content className='lg:items-stretch'>
+    <Container className='lg:items-stretch'>
       <Row justifyXs='center'>
         <Column md={6}>
-          <h1 className='font-orelega mb-4 text-5xl text-center text-brown'>
-            Over a decade of crafting intuitive digital experiences
-          </h1>
+          <ContentTransition>
+            <h1 className='font-orelega my-12 text-5xl text-center text-brown'>
+              Over a decade of crafting intuitive digital experiences
+            </h1>
+          </ContentTransition>
         </Column>
       </Row>
       <Row>
-        {caseStudyEntries.map((caseStudy) => (
+        {caseStudyEntries.map((caseStudy, index) => (
           <Column md={4} key={caseStudy.link}>
-            <CaseStudyCard 
-              img={{
-                src: caseStudy.image,
-                alt: caseStudy.title
-              }}
-              title={caseStudy.title}
-              cta={{
-                text: "View Project",
-                href: caseStudy.link
-              }}
-            />
+            <ContentTransition delay={0.3 + index * 0.2}>
+              <CaseStudyCard 
+                img={{
+                  src: caseStudy.image,
+                  alt: caseStudy.title
+                }}
+                title={caseStudy.title}
+                cta={{
+                  text: "View Project",
+                  href: caseStudy.link
+                }}
+              />
+            </ContentTransition>
           </Column>
         ))}
       </Row>  
-    </Content>
+    </Container>
   )
 }
 
--- app/components/grid/container.tsx
+++ app/components/grid/container.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import { ReactNode } from 'react'
-import { usePathname } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
-import clsx from 'clsx'
+import PageTransition from '@/components/motion/page-transition'
 
-type ContainerProps = {
+type ContainerProps = { 
   className?: string;
   children: ReactNode;
 }
@@ -19,9 +18,9 @@ const Container = ({ className = '', children }: ContainerProps) => {
         className
       )
     }>
-      <div className='pt-8 px-6 sm:flex-auto md:pb-10 md:px-16'>
+      <PageTransition>
         {children}
-      </div>
+      </PageTransition>
     </main>
   )
 }
