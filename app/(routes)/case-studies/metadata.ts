6f00f64d9fe3fb0ee24478da3f30ba4297738d6e
Juan D. Bolaños <juanb@aweber.com>
2025-03-02 15:01:36 -0500

    export case studies metadata

---
+++ app/(routes)/case-studies/metadata.ts
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Case Studies',
+  description: 'A collection of case studies, component specs, and projects that showcase my more than a decades worth of experience in the design industry.',
+  openGraph: {
+    title: 'Case Studies',
+    description: 'A collection of case studies, component specs, and projects that showcase my more than a decades worth of experience in the design industry.'
+  }
+} 
